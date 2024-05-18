490a4adc8f6c6909bee66e4194c31eebe59a1491
deepak <deepakkumardash313@gmail.com>
2024-05-18 20:39:34 +0530

    added index page design

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,89 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { useAssets } from "expo-asset";
+import { ResizeMode, Video } from "expo-av";
+import { Link } from "expo-router";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
+  const [assets] = useAssets([require("@/assets/videos/intro.mp4")]);
+
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      {assets && (
+        <Video
+          isMuted
+          isLooping
+          shouldPlay
+          resizeMode={ResizeMode.COVER}
+          source={{ uri: assets[0].uri }}
+          style={styles.video}
+        />
+      )}
+
+      <View style={{ marginTop: 80, padding: 20 }}>
+        <Text style={styles.header}>
+          Ready to take control of your finances?
+        </Text>
+      </View>
+
+      <View style={styles.button}>
+        <Link
+          href={"/login"}
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: Colors.background },
+          ]}
+          asChild
+        >
+          <TouchableOpacity>
+            <Text style={{ fontSize: 22, fontWeight: 500 }}>Login</Text>
+          </TouchableOpacity>
+        </Link>
+
+        <Link
+          href={"/signup"}
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: Colors.primary },
+          ]}
+          asChild
+        >
+          <TouchableOpacity>
+            <Text style={{ fontSize: 22, fontWeight: 500, color: "white" }}>
+              Signup
+            </Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
     </View>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  header: {
+    fontSize: 36,
+    fontWeight: "900",
+    color: "white",
+    textTransform: "uppercase",
+  },
+  button: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 60,
+    paddingHorizontal: 20,
+    gap: 20,
+  },
+});
